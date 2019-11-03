3be07fbe77f3ebb347e7c45c47bf98a72d552c7c
Wael <waelsy123@gmail.com>
2019-11-03 20:05:43 +0100

    user heroku PORT env var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ejs = require('ejs')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
